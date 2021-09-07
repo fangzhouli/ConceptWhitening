4aa6244b9bb5a0e8a3c0654decdd53d91704c756
fangzhouli <fzli0805@gmail.com>
2021-09-07 11:42:16 -0700

    added scrips for slur,m

--- scripts/slurm_train_places_resnet18_concept_whitening_transfer.sh
+++ scripts/slurm_train_places_resnet18_concept_whitening_transfer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --job-name=concept-coloring
+#SBATCH --job-name=cc
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=fzli@ucdavis.edu
 #SBATCH --output=/share/taglab/Fang/concept-coloring-preliminary/logs/%j.out
@@ -8,7 +8,8 @@
 #SBATCH --partition=production
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=cuda:7.5
+#SBATCH --gres=cuda:6.0
+#SBATCH --cpus-per-task=32
 #SBATCH --time=1-00:00:00
 
-python ../train_places.py --ngpu 1 --workers 32 --arch resnet_cw --depth 18 --epochs 200 --batch-size 64 --lr 0.05 --whitened_layers 7 --concepts airplane,bicycle,car,train,dining_table,microwave,oven,cat,dog --prefix RESNET18_PLACES365_TRANSFER_CW /share/taglab/Fang/concept-coloring-preliminary/data/
+python ../train_places.py --ngpu 1 --workers 32 --arch resnet_cw --depth 18 --epochs 200 --batch-size 64 --lr 0.05 --whitened_layers 7 --concepts airplane,bicycle,car,train,dining_table,microwave,oven,cat,dog --prefix RESNET18_PLACES365_TRANSFER_CC /share/taglab/Fang/cw/data_256/
