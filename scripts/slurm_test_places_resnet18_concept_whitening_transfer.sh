926c4e317d89f82d3005c23632d86b0eb5dde47a
fangzhouli <fzli0805@gmail.com>
2021-09-20 10:41:58 -0700

    refactored: better looking commands; added: checkpoint timestamp; fixed: slurm partition

--- scripts/slurm_test_places_resnet18_concept_whitening_transfer.sh
+++ scripts/slurm_test_places_resnet18_concept_whitening_transfer.sh
@@ -8,10 +8,27 @@
 #SBATCH --partition=dev
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=64
 #SBATCH --gres=cuda:6.1
+#SBATCH --mem=300G
 #SBATCH --time=3:00:00
-##SBATCH --partition=production
-##SBATCH --time=1-00:00:00
 
-python ../train_places.py --seed 1232 --ngpu 1 --workers 32 --arch resnet_cw --depth 18 --epochs 200 --batch-size 1 --lr 0.1 --whitened_layers 7 --concepts airplane,bicycle,car,train,dining_table,microwave,oven,cat,dog --prefix RESNET18_PLACES365_TRANSFER_CC /share/taglab/Fang/cw/data_256/ --evaluate
+## Timestmaps: 20210903_073210
+##             20210904_115816
+##             20210904_205429
+##             20210905_131108
+
+python ../train_places.py /share/taglab/Fang/cw/data_256/ \
+    --seed 1232 \
+    --ngpu 1 \
+    --workers 60 \
+    --arch resnet_cw \
+    --depth 18 \
+    --epochs 200 \
+    --batch-size 1 \
+    --lr 0.1 \
+    --whitened_layers 7 \
+    --concepts airplane,bicycle,car,train,dining_table,microwave,oven,cat,dog \
+    --checkpoint-timestamp 20210903_073210 \
+    --prefix RESNET18_PLACES365_TRANSFER_CC \
+    --evaluate
